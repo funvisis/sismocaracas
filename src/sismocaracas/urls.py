9f9ac4b3bf1edc8031817396e839e38af17c11b1
Jesús Gómez <jgomez@funvisis.gob.ve>
2011-08-31 13:27:19 -0430

    Habilitado el site administrativo para usuarios y grupos.

--- src/sismocaracas/urls.py
+++ src/sismocaracas/urls.py
@@ -1,7 +1,15 @@
 from django.conf.urls.defaults import patterns, include, url
 
+from django.contrib import auth
+from django.contrib import admin
+
+admin.site.register(auth.models.Group)
+admin.site.register(auth.models.User)
+#admin.autodiscover()
+
 urlpatterns = patterns(
     '',
     url(r'^edificaciones/', include('sismocaracas.buildings.urls')),
-    url(r'^puentes/', include('sismocaracas.bridges.urls'))
+    url(r'^puentes/', include('sismocaracas.bridges.urls')),
+    (r'^admin/', include(admin.site.urls)),
     )
