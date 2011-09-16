30478541d7faf5fd33aa7366621c289e243bce1a
Jesús Gómez <jgomez@funvisis.gob.ve>
2011-09-16 11:07:24 -0430

    Vista csv_view agregada. Genera un archivo csv con todos los elementos del modelo consultado.

--- src/sismocaracas/structuralinspections/urls.py
+++ src/sismocaracas/structuralinspections/urls.py
@@ -3,7 +3,10 @@ from .admin import admin_site
 # from .admin.revisor import  revisor_admin_site
 # from .admin.inspector import  inspector_admin_site
 
+from .views import csv_view
+
 urlpatterns = patterns(
     '',
+    url(r'^csv/(?P<models_url>\w+)/', csv_view),
     url(r'', include(admin_site.urls)),
     )
--- src/sismocaracas/structuralinspections/views.py
+++ src/sismocaracas/structuralinspections/views.py
@@ -1,1 +1,33 @@
+# -*- coding: utf-8 -*-
 # Create your views here.
+
+import csv
+from django.http import HttpResponse
+from .models import Building, Bridge
+
+model_url_dict = {
+    'edificaciones': Building,
+    'puentes':Bridge}
+
+def csv_view(request, models_url='edificaciones'):
+    # Create the HttpResponse object with the appropriate CSV header.
+    response = HttpResponse(mimetype='text/csv; charset=utf-8')
+    response['Content-Disposition'] = \
+        'attachment; filename=inspecciones_{0}.csv'.format(models_url)
+
+    writer = csv.writer(response)
+
+    model = model_url_dict[models_url]
+
+    field_names = model._meta.get_all_field_names()
+
+    writer.writerow([
+            model._meta.get_field(field_name).verbose_name.encode('utf-8')
+            for field_name in field_names])
+
+    for element in model.objects.all():
+        writer.writerow([
+                unicode(getattr(element, field_name)).encode('utf-8')
+                for field_name in field_names])
+
+    return response
